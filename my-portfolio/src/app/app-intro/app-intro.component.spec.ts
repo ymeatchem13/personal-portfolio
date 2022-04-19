dbeb3505750163b47da93c87c27ea4c253568785
Yoshua Meatchem <meatchemyoshua@yahoo.com>
2022-04-19 17:56:35 -0400

    created app-intro component

---
+++ my-portfolio/src/app/app-intro/app-intro.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppIntroComponent } from './app-intro.component';
+
+describe('AppIntroComponent', () => {
+  let component: AppIntroComponent;
+  let fixture: ComponentFixture<AppIntroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppIntroComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppIntroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
